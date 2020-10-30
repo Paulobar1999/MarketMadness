bdf23c5ea8ee5f781bfea479973d7cd90d849598
Paulobar1999 <paulobar1999@gmail.com>
2020-10-30 12:59:53 -0500

    Game Controller

---
+++ MarketMadness/src/application/GameController.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+
+
+
+public class GameController {
+
+    @FXML
+    private Button bankruptcyButton;
+
+    @FXML
+	void Bankruptcy(ActionEvent event) throws IOException {
+		BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Scene scene = new Scene(root);
+		Main.primaryStage.setScene(scene);
+	}
+    
+    @FXML
+    void handle(ActionEvent event) {
+
+    }
+
+}
