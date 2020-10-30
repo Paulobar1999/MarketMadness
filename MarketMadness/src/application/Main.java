2e3a4704e2e92847c34733046a04adb85ae40853
Paulobar1999 <paulobar1999@gmail.com>
2020-10-30 03:12:01 -0500

    Changed UI to BorderPane, Connected Main.fxml to Game.fxml

--- MarketMadness/src/application/Main.java
+++ MarketMadness/src/application/Main.java
@@ -1,27 +1,32 @@
 package application;
 	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	
+	public static Stage primaryStage;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(root,800,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			this.primaryStage=primaryStage;
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
