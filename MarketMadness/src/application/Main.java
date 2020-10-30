01d3124d1f9386268cc52c053b8b8a22821d1f0d
Paulobar1999 <paulobar1999@gmail.com>
2020-10-30 02:45:41 -0500

    Updated UI

--- MarketMadness/src/application/Main.java
+++ MarketMadness/src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -11,8 +12,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,800,800);
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root,800,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
