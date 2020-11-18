b672a86017f53fe45ba0499c9b49e46ffca6ef03
Paulobar1999 <paulobar1999@gmail.com>
2020-11-18 09:22:46 -0600

    Formmated code

--- MarketMadness/src/application/Main.java
+++ MarketMadness/src/application/Main.java
@@ -1,5 +1,4 @@
 package application;
-	
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -7,26 +6,26 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
-
 public class Main extends Application {
-	
+
 	public static Stage primaryStage;
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,841,731);
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root, 841, 731);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			Main.primaryStage=primaryStage;
+			Main.primaryStage = primaryStage;
 			primaryStage.setTitle("MarketMadness");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- MarketMadness/src/application/MainController.java
+++ MarketMadness/src/application/MainController.java
@@ -10,47 +10,47 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-
-
-
 public class MainController {
-	
-    @FXML private Button playButton;
-    @FXML private Button creditsButton;
-	@FXML private Button ExitButton;
-	
-    @FXML
+
+	@FXML
+	private Button playButton;
+	@FXML
+	private Button creditsButton;
+	@FXML
+	private Button ExitButton;
+
+	@FXML
 	void GameStart(ActionEvent event) throws IOException {
 		BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("Game.fxml"));
 		Scene scene = new Scene(root);
 		Main.primaryStage.setScene(scene);
 	}
-    
-    @FXML
-    void GoToCredits(ActionEvent event) throws IOException {
-    	//get 'Credits' button to work.
-    	BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("Credits.fxml"));
+
+	@FXML
+	void GoToCredits(ActionEvent event) throws IOException {
+		// get 'Credits' button to work.
+		BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("Credits.fxml"));
 		Scene scene = new Scene(root);
 		Main.primaryStage.setScene(scene);
-    }
-    
-    @FXML 
-    void BackToMainMenu(ActionEvent event) throws IOException {
-		BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+	}
+
+	@FXML
+	void BackToMainMenu(ActionEvent event) throws IOException {
+		BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("Main.fxml"));
 
 		Scene scene = new Scene(root);
 		Main.primaryStage.setScene(scene);
-    }
-    
+	}
+
 	@FXML
 	void Exit(ActionEvent event) throws IOException {
 		Stage stage = (Stage) ExitButton.getScene().getWindow();
 		stage.close();
 	}
-    
-    @FXML
-    void handle(ActionEvent event) {
 
-    }
+	@FXML
+	void handle(ActionEvent event) {
+
+	}
 
 }
